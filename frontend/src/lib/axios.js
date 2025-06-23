8970d18e6fd36e1ed38175e428ee402bf010e7f4
DhanushDhanush2001 <dhanushpandian.0207@gmail.com>
2025-06-23 11:38:49 +0530

    updated axios url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
+import axios from "axios";
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:8000/api"
-      : "https://fullstack-chat-app-n871.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api",
   withCredentials: true,
 });
