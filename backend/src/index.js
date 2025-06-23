4e22c9429fd5a06f409b9d613e6ec6586d95cf51
DhanushDhanush2001 <dhanushpandian.0207@gmail.com>
2025-06-23 11:01:15 +0530

    fix: remove duplicate express declaration in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,16 +5,14 @@ const cors = require('cors');
 const connectDB = require('./lib/db');
 const authRoutes = require('./routes/auth.route');
 const messageRoutes = require('./routes/message.route');
-const {app, server} = require('./lib/socket');
+const { app, server } = require('./lib/socket');
 const path = require('path');
-const express = require("express");
 
-
-dotenv.config()
+dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -23,10 +21,8 @@ app.use(
   })
 );
 
-
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -36,8 +32,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
-server.listen(PORT,()=>{
-    console.log("server is running on PORT:"+PORT);
-    connectDB()
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
